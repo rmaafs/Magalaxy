16de9dc047ad21cd77ae3c0a8ac6ae03e2e3472a
Rodrigo Maafs <rodrigo@ticketfactura.com>
2019-04-17 02:47:45 -0500

    Initial commit

---
+++ android/src/com/relmaps/magalaxy/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.relmaps.magalaxy.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.relmaps.magalaxy.InitGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new InitGame(), config);
+	}
+}
---
+++ desktop/src/com/relmaps/magalaxy/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.relmaps.magalaxy.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.relmaps.magalaxy.InitGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new InitGame(), config);
+	}
+}
