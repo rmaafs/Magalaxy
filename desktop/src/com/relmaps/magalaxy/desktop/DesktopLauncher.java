63b3ffbc371ffa9314de604715610557adccbf19
Rodrigo Maafs <rodrigo@ticketfactura.com>
2019-06-06 21:45:13 -0500

    Edité el nombre y logo de la ventana del juego.

--- desktop/src/com/relmaps/magalaxy/desktop/DesktopLauncher.java
+++ desktop/src/com/relmaps/magalaxy/desktop/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package com.relmaps.magalaxy.desktop;
 
+import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.relmaps.magalaxy.InitGame;
@@ -10,6 +11,8 @@ public class DesktopLauncher {
 		config.width = 1024;
 		config.height = 640;
 		config.foregroundFPS = 60;
+		config.title = "MaGalaxy";
+		config.addIcon("logo.png", Files.FileType.Internal);
 		new LwjglApplication(new InitGame(), config);
 	}
 }
